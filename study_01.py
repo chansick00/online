1f49b0ce37be8b2d4fb06cb36554e8953bcadd20
chansick00 <wonju0318@naver.com>
2022-01-27 15:42:45 +0900

    test01

---
+++ study_01.py
@@ -0,0 +1,21 @@
+# print("hello")
+# 이제 여기서부터 시작하면 됩니다.
+
+# 숫자 1,2,3,-1, 0.234
+
+# 정수를 'int' --> integer
+# 실수를 'float'
+
+# 더하기, 빼기, 나누기, 곱하기
+
+
+a = 2
+b = 3
+
+c = a * b
+print(c)
+print(2 * 3)
+print('2' * 3)
+
+print(type(2))
+print(type('2'))
